8b8917ecc3c3b0b421a069b269ed0eb18aec2094
Roshan Nyaupane <106418513+RNyaupane@users.noreply.github.com>
2024-01-22 21:50:28 +0545

    folder structure added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+      <h1 className="text-black">Text</h1>
+    </>
+  );
+}
+
+export default App;
